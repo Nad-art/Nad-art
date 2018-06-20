487ff5ecc446a4b7fa912361b749e51772fe6784
gaelleF <gaelle.fougere@outlook.fr>
2018-06-20 17:14:10 +0200

    ajout router

--- app-react-nadart/src/App.js
+++ app-react-nadart/src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import Home from './components/Home-1.js'
+import Communaute from './components/Communaute.js'
 import 'reset-css'
 import './style/App.css'
+import { Router, Link } from '@reach/router'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+
+        <Router>
+          <Home path='/' />
+          <Communaute path='communaute' />
+        </Router>
+
+        <nav>
+          <Link to='/' > Accueil </Link>||{" "} 
+          <Link to='communaute'> Communauté</Link>
+        </nav>
       </div>
     );
   }
---
+++ app-react-nadart/src/components/Communaute.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../style/Home-1.css'
+
+const Communaute = () => {
+    return (
+        <div> Page communauté! </div>
+    )
+
+}
+
+export default Communaute
