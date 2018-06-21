d0e17fa9ece5953c8a0156d1433eb8c73371d9cc
SophieMdl <sophie.michaudel@gmail.com>
2018-06-21 13:37:46 +0200

    Slide-avancement

--- app-react-nadart/src/components/Slider.js
+++ app-react-nadart/src/components/Slider.js
@@ -1,68 +1,83 @@
-import React from "react";
-import MobileStepper from "@material-ui/core/MobileStepper";
-import SwipeableViews from "react-swipeable-views";
-import "../styles/Slider.css"
-import imageNadarPath from '../images/nadar_bg.png'
+import React from 'react';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import SwipeableViews from 'react-swipeable-views';
+import Dates from './Dates.js';
+import '../styles/Slider.css';
+import imageNadar from '../images/nadar_bg.png';
+import imageSarah from '../images/sarah_bernardt.png';
 
-const tutorialSteps = [
+const images = [
     {
-        label: "Jouer avant l'exposition",
-        imgPath: imageNadarPath
+        title: 'Exposition Nadar',
+        label: 'à la BNF Paris',
+        imgPath: imageSarah,
     },
     {
-        label: "Exposition Nadar",
-        imgPath: "http://placekitten.com/500/400"
-    }
-]
+        title: "Jouer avant l'exposition",
+        label: 'Prenez-vous en selfie à la manière de Nadar pour remporter des offres',
+        imgPath: imageNadar,
+    },
+];
+const theme = {
+    mobileStepper: {
+        backgroundColor: 'transparent',
+        position: 'absolute',
+        left: 'calc(50% - 22px)',
+    },
+};
 
 class Slider extends React.Component {
     state = {
-        activeStep: 0
-    }
+        activeStep: 0,
+    };
 
     handleNext = () => {
         this.setState(prevState => ({
-            activeStep: prevState.activeStep + 1
-        }))
-    }
+            activeStep: prevState.activeStep + 1,
+        }));
+    };
 
     handleBack = () => {
         this.setState(prevState => ({
-            activeStep: prevState.activeStep - 1
-        }))
-    }
+            activeStep: prevState.activeStep - 1,
+        }));
+    };
 
     handleStepChange = activeStep => {
         this.setState({ activeStep });
-    }
+    };
 
     render() {
         const { activeStep } = this.state;
 
-        const maxSteps = tutorialSteps.length;
+        const maxSteps = images.length;
 
         return (
             <div className="slider">
                 <MobileStepper
+                    style={theme.mobileStepper}
                     steps={maxSteps}
                     position="static"
                     activeStep={activeStep}
                 />
+                <div className="content-slide">
+                    {activeStep === 0 ? <Dates /> : null}
+                    <h3>{images[activeStep].title}</h3>
+                    <span>{images[activeStep].label}</span>
+                </div>
+
                 <SwipeableViews
                     className="slider-content"
                     index={this.state.activeStep}
                     onChangeIndex={this.handleStepChange}
                     enableMouseEvents
                 >
-                    {tutorialSteps.map(step => (
-                        <img key={step.label} src={step.imgPath} alt={step.label} />
-                    ))}
+                    {images.map(image => <img src={image.imgPath} />)}
                 </SwipeableViews>
 
-
             </div>
-        )
+        );
     }
 }
 
-export default Slider
+export default Slider;
