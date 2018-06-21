85747aaa510d6ebfd9df1bd48322f87a1f9a0057
gaelleF <gaelle.fougere@outlook.fr>
2018-06-21 18:17:59 +0200

    modif css sur challenge

--- app-react-nadart/src/components/Challenge.js
+++ app-react-nadart/src/components/Challenge.js
@@ -13,17 +13,18 @@ const Challenge = () => {
               <div className="button-photo"> Prendre une nouvelle photo </div>
               <p> Plus que <span className='photo-span'> 3 photos </span></p>
               <p>pour debloquer le billet pour la visite à 12€</p>
+              <div className='selfie-challenge'>
+                 <div className='selfie-capture'>  </div>
+                 <div className='selfie-capture'> </div>
+                 <div className='selfie-capture'>  </div>
+                 <div className='selfie-capture'>  </div>
+            </div>
             </div>
 
 
             <div className='extension-photo'>
             {/*<img className='extension-photo' src={maevaPicture} alt='maeva' />*/}
-            <div className='selfie-challenge'>
-                 <div className='selfie-capture'> photo 1 </div>
-                 <div className='selfie-capture'> photo 2 </div>
-                 <div className='selfie-capture'> photo 3 </div>
-                 <div className='selfie-capture'> photo 4 </div>
-            </div>
+            
             <div className='desc-selfie' />
                 <h4> Photo 1 </h4>
                 <p> 12 juin 2018</p>
