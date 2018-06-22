c0b597598e62d62c51e9e6401a18f2259b599dfb
kyerz <mikael.versu@gmail.com>
2018-06-22 14:17:07 +0200

    add fb & google icons on home menu

--- app-react-nadart/src/components/ConnexionButtons.js
+++ app-react-nadart/src/components/ConnexionButtons.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import { Link } from '@reach/router'
 import '../styles/ConnexionButtons.css'
+import facebook from '../images/facebook.png'
+import google from '../images/google.png'
 
 const buttons = {
     general : {
@@ -23,9 +25,11 @@ const ConnexionButtons = () => {
         <div className="buttons-container">
             <Link to="/home"> 
                 <Button variant="contained" style={buttons.white}>
-                    Connexion Facebook
+                    <img className='facebook' src={facebook} alt="#"/>
+                        <span className='fb-text'>Connexion Facebook</span>
                 </Button>
                 <Button variant="contained" style={buttons.black}>
+                <img src={google} alt="#" className='google'/>
                     Connexion Google
                 </Button>
             </Link>
